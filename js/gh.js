249006cdbbc521306182e8ab9da89af938213af4
Kieran Topping <kieran.topping@energycap.com>
2025-02-20 09:05:34 +0000

    WIP

--- js/gh.js
+++ js/gh.js
@@ -1,153 +1,155 @@
-(function() {
+if (window.ktoBookmarklet) {
+	window.ktoBookmarklet.toggleMenu();
+} else {
 
-    var ktoBookmarklet = {menuDisplayed: false};
+	(function() {
 
-    var test = document.location.protocol === 'file:' || document.location.host === 'kierantop.github.io';
-    if (!test && document.location.host !== 'github.com') {
-        alert('Only run on github.com');
-        return false;
-    }
+		var kto = {menuDisplayed: false};
+		var document = window.document;
 
-    var showMessage = (msg) => {
-        alert(msg);
-    }
+		var test = document.location.protocol === 'file:' || document.location.host === 'kierantop.github.io';
+		if (!test && document.location.host !== 'github.com') {
+			alert('Only run on github.com');
+			return false;
+		}
 
-    var rewriteImages = function () {
+		var showMessage = (msg) => {
+			alert(msg);
+		};
 
-        var t = document.activeElement;
-        if (!(t && t.type === 'textarea')) {
-            showMessage('Please focus on a textarea');
-            return false;
-        }
+		var getActiveTA = () => {
+			var t = document.activeElement;
+			if (!(t && t.type === 'textarea')) {
+				showMessage('Please focus on a textarea');
+				throw('abc');
+			}
+			return [t, t.selectionStart, t.selectionEnd];
+		};
 
-        var s = t.selectionStart;
-        var e = t.selectionEnd;
-        var msg = [];
-        var _in = 'selection';
-        if (!(s!==undefined && e!==undefined && e-s > 0)) {
-            t.focus(); document.execCommand('selectAll', false);
-            s = t.selectionStart;
-            e = t.selectionEnd;
-            _in = 'entire textarea';
-        }
-        var sel = t.value.substring(s, e);
-    
-        var c = 0;
-        sel = sel.replaceAll(
-            /\!\[image\]\((https:\/\/github\.com\/user-attachments\/.*?)\)/g,
-            (s, p1) => { c+=1; return '<img width=30% src=' + p1 + '>'; }
-        );
-        msg.push('Replaced ' + c + ' occurence(s) of ![image](...) in ' + _in);
-        /* Apparently, deprecated, but it's the only way to preserve undo buffer */
-        if (document.execCommand) {
-            document.execCommand('insertText', false, sel);
-        } else {
-            msg.push('Beware, your undo buffer might misbehave.');
-            t.value = t.value.substring(0, s) + sel + t.value.substring(e);
-        }
-        showMessage(msg.join(' '), t.parentElement, t.offsetWidth + 'px');
-    };
+		var rewriteImages = function () {
 
-    var insert = (name, str) => {
-        
-        var t = document.activeElement;
-        if (!(t && t.type === 'textarea')) {
-            showMessage('Please focus on a textarea');
-            return false;
-        }
+			var t, s, e = getActiveTA();
 
-        var s = t.selectionStart;
-        var e = t.selectionEnd;
+			var msg = [];
+			var _in = 'selection';
+			if (!(s!==undefined && e!==undefined && e-s > 0)) {
+				t.focus(); document.execCommand('selectAll', false);
+				s = t.selectionStart;
+				e = t.selectionEnd;
+				_in = 'entire textarea';
+			}
+			var sel = t.value.substring(s, e);
+		
+			var c = 0;
+			sel = sel.replaceAll(
+				/\!\[image\]\((https:\/\/github\.com\/user-attachments\/.*?)\)/g,
+				(s, p1) => { c+=1; return '<img width=30% src=' + p1 + '>'; }
+			);
+			msg.push('Replaced ' + c + ' occurence(s) of ![image](...) in ' + _in);
+			/* Apparently, deprecated, but it's the only way to preserve undo buffer */
+			if (document.execCommand) {
+				document.execCommand('insertText', false, sel);
+			} else {
+				msg.push('Beware, your undo buffer might misbehave.');
+				t.value = t.value.substring(0, s) + sel + t.value.substring(e);
+			}
+			showMessage(msg.join(' '), t.parentElement, t.offsetWidth + 'px');
+		};
 
-        str = str.replaceAll(/\r?\n/g, "\r\n");
+	
+		var insert = (name, str) => {
+			
+			var t, s, e = getActiveTA();
 
-        var msg = [];
+			var msg = [];
 
-        /* Apparently, deprecated, but it's the only way to preserve undo buffer */
-        if (document.execCommand) {
-            document.execCommand('insertText', false, str);
-        } else {
-            t.value = t.value.substring(0, s) + str + t.value.substring(e);
-            msg.push('Beware, your undo buffer might misbehave.');
-        }
-        msg.push('Inserted ' + name + ' at cursor');
-        showMessage(msg.join(' '));
-    };
+			/* Apparently, deprecated, but it's the only way to preserve undo buffer */
+			if (document.execCommand) {
+				document.execCommand('insertText', false, str);
+			} else {
+				t.value = t.value.substring(0, s) + str + t.value.substring(e);
+				msg.push('Beware, your undo buffer might misbehave.');
+			}
+			msg.push('Inserted ' + name + ' at cursor');
+			showMessage(msg.join(' '));
+		};
 
-    var insertDetails = () => {
-        insert("details", `<details>
-    <summary>
-        summary
-    </summary>
-    details
-</details>
-`);
-    }
+		var rpl = (s) => s.replaceAll(/\t/g,'    ').replaceAll(/\r?\n/g, '\r\n');
 
-    var insertTable = () => {
-        insert("table", `<table>
-    <tr>
-        <th>
-        head1
-        </th>
-        <th>
-        head2
-        </th>
-    </tr>
-    <tr>
-        <td>
-        cell1
-        </td>
-        <td>
-        cell2
-        </td>
-    </tr>
-</table>`);
-    };
+		var insertDetails = () => {
+			insert('details', rpl(`<details>
+		<summary>
+			summary
+		</summary>
+		details
+	</details>
+`));
+		};
 
-    var createLink = (name, fn) => {
-        var a = document.createElement("a");
-        a.setAttribute('href', '#');
-        a.textContent = name;
-        // Catch clicks without grabbing focus
-        a.onmousedown = function(event) { event.preventDefault(); };
-        a.onmouseup = function(event) {
-            event.preventDefault();
-            fn();
-        }
-        return a;
-    };
+		var insertTable = () => {
+			insert('table', rpl(`<table>
+	<tr>
+		<th>
+			head1
+		</th>
+		<th>
+			head2
+		</th>
+	</tr>
+	<tr>
+		<td>
+			cell1
+		</td>
+		<td>
+			cell2
+		</td>
+	</tr>
+</table>`));
+		};
 
-    var showMenu = () => {
-        var wrapper = document.createElement('div');
-        wrapper.setAttribute('id', 'ktoBookmarklet');
-        wrapper.setAttribute('style', 'width:100%;height:0;position:fixed;top:0;left:0;z-index: 1000;');
-        var content = document.createElement('div');
-        content.setAttribute('style', 'color:#17303B;margin: auto;text-align: center;padding:  10px;background-color:#eee;border:2px solid #A0AD39;opacity: 0.9;font-family:arial');
-        content.textContent="v0.1 | ";
-        content.append(createLink("<img>", rewriteImages));
-        content.append(" | ");
-        content.append(createLink("<table>", insertTable));
-        content.append(" | ");
-        content.append(createLink("<details>", insertDetails));
-        wrapper.appendChild(content);
-        document.body.append(wrapper);
-        ktoBookmarklet.menuDisplayed = true;
-    };
+		var createLink = (name, fn) => {
+			var a = document.createElement('a');
+			a.setAttribute('href', '#');
+			a.textContent = name;
+			/* Catch clicks without grabbing focus */
+			a.onmousedown = function(e) { e.preventDefault(); };
+			a.onmouseup = function(e) {
+				e.preventDefault();
+				fn();
+			};
+			return a;
+		};
 
-    var hideMenu = () => {
-        document.getElementById("ktoBookmarklet").remove();
-        ktoBookmarklet.menuDisplayed = false;
-    };
+		var showMenu = () => {
+			var w = kto.el = document.createElement('div');
+			w.setAttribute('style', 'width:100%;height:0;position:fixed;top:0;left:0;z-index: 1000;');
+			var c = document.createElement('div');
+			c.setAttribute('style', 'color:#17303B;margin: auto;text-align: center;padding:  10px;background-color:#eee;border:2px solid #A0AD39;opacity: 0.9;font-family:arial');
+			c.textContent='v0.1 | ';
+			c.append(createLink('<img>', rewriteImages));
+			c.append(' | ');
+			c.append(createLink('<table>', insertTable));
+			c.append(' | ');
+			c.append(createLink('<details>', insertDetails));
+			w.appendChild(c);
+			document.body.append(w);
+			kto.menuDisplayed = true;
+		};
 
-    var toggleMenu = () => {
-        if (ktoBookmarklet.menuDisplayed) hideMenu();
-        else showMenu();
-    };
+		var hideMenu = () => {
+			kto.el.remove();
+			kto.menuDisplayed = false;
+		};
 
-    ktoBookmarklet.toggleMenu = toggleMenu;
+		var toggleMenu = () => {
+			if (kto.menuDisplayed) hideMenu();
+			else showMenu();
+		};
 
-    toggleMenu();
+		kto.toggleMenu = toggleMenu;
 
-    window.ktoBookmarklet = ktoBookmarklet;
-})();
+		toggleMenu();
+
+		window.ktoBookmarklet = kto;
+	})();
+}
